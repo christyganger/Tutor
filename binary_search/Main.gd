e8ddc8659892bcb64d954897dff52a891f0c6d8d
christyganger <christyganger@gmail.com>
2018-10-25 14:38:55 -0700

    prettyfying
    yay

--- binary_search/Main.gd
+++ binary_search/Main.gd
@@ -2,7 +2,10 @@ extends Control
 
 # Get node
 onready var highest_number_textbox = get_node("Label2/TextEdit")
-
+onready var highest_number_current = get_node("current_number/TextEdit")
+onready var user_current_guess = get_node("user_guesses_current/current__user_guesses_textbox")
+onready var pc_current_guess = get_node("pc_guesses_current/current_pc_guesses")
+var text_change_flag = false
 func _ready():
 	get_node("Polygon2D/Label/Buttonguessmain").set_text("You guess the Program's number")
 	get_node("Polygon2D/Label/Buttonpcfiguremain").set_text("Program guesses your number")
@@ -14,10 +17,7 @@ func _on_Buttonpcfiguremain_pressed():
 	# Initialization here
 		pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+
 
 
 func _on_Buttonguessmain_pressed():
@@ -26,7 +26,23 @@ func _on_Buttonguessmain_pressed():
 
 
 func _on_TextEdit_text_changed():
+	
 	global.user_input_highest = int(highest_number_textbox.get_text())
 	if global.user_input_highest >= 1:
 		global.randomnumber = int(rand_range(1,global.user_input_highest))
+	
+	pass # replace with function body
+
+func _process(delta):
+	
+	highest_number_current.set_text(String(global.user_input_highest))
+	user_current_guess.set_text(String(global.amount_of_user_guesses))
+	pc_current_guess.set_text(String(global.guess_count))
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	pass
+
+func _on_Button_pressed():
+	global.guess_count = 0
+	global.amount_of_user_guesses = 0
 	pass # replace with function body
